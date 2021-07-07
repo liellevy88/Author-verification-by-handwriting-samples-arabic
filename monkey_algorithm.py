3f81b2c7e805f4ec77062fb68b6e543f036bb6fc
avigailsh <avigail838@gmail.com>
2021-07-07 21:26:05 +0300

    final version

--- monkey_algorithm.py
+++ monkey_algorithm.py
@@ -1,17 +1,16 @@
-from neural_network_for_monkey_alg import neural_network
+from neural_network_monkey_algorithm import test_nn
+
 
 array_of_diff_vectors = []
-two_vecs = []  # the 2 vectors of the 2 files
+two_vecs = [0]*2  # the 2 vectors of the 2 files
 
 
 def neural_network_returned_vec(letters_directories_name):
-    for i in range(2):
-        vec = neural_network(letters_directories_name[i], i)
-        two_vecs.append(vec)
-        # print(two_vecs)
+    vec1, vec2 = test_nn(letters_directories_name[0], letters_directories_name[1])
+    two_vecs[0] = vec1
+    two_vecs[1] = vec2
     diff_vector = create_diff_vector(two_vecs[0], two_vecs[1])
     array_of_diff_vectors.append(diff_vector)  # just for train
-    # logistic_reg(diff_vector)
 
 
 def create_diff_vector(vec_1, vec_2):
@@ -20,7 +19,4 @@ def create_diff_vector(vec_1, vec_2):
         diff_vector[i] = abs(vec_1[i] - vec_2[i])
     return diff_vector
 
-
-# def logistic_reg(diff_vector):
-#     y_pred = logreg.predict(X_val_temp)
 
